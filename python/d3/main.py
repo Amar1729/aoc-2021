68a44a722100ebc50c691ece0ac95d16a7c92803
Amar1729 <amar.paul16@gmail.com>
2021-12-05 01:57:10 -0500

    d3p2

--- python/d3/main.py
+++ python/d3/main.py
@@ -23,12 +23,55 @@ def p1(lines):
 
 
 def p2(lines):
-    pass
+    o2 = None
+    co2 = None
+
+    o2_nums = [l.strip() for l in lines]
+    co2_nums = [l.strip() for l in lines]
+
+    def partition(step, lines, tiebreak):
+        if not lines:
+            # return []
+            pass
+        freq = collections.defaultdict(int)
+        column = list(zip(*lines))[step]
+        for bit in column:
+            freq[bit] += 1
+
+        if freq["0"] > freq["1"]:
+            most_common = "0"
+        # elif freq["1"] >= freq["0"]:
+        else:
+            most_common = "1"
+
+        if tiebreak == "1":
+            return list(filter(lambda l: l[step] == most_common, lines))
+        else:
+            return list(filter(lambda l: l[step] != most_common, lines))
+
+    for step in range(len(o2_nums[0])):
+
+        if not o2:
+            o2_nums = partition(step, o2_nums, "1")
+
+            if len(o2_nums) == 1:
+                o2 = o2_nums[0]
+
+        if not co2:
+            co2_nums = partition(step, co2_nums, "0")
+
+            if len(co2_nums) == 1:
+                co2 = co2_nums[0]
+
+        if o2 and co2:
+            break
+
+    return int(o2, 2) * int(co2, 2)
 
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         lines = f.readlines()
 
-    print(p1(lines))
-    # print(p2(lines))
+    # print(p1(lines))
+    print(p2(lines))
