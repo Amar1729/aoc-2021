e54c67f4180f756f1da311cc66faeb541a66a8f0
Amar1729 <amar.paul16@gmail.com>
2021-12-12 01:09:19 -0500

    d12p2

--- python/d12/main.py
+++ python/d12/main.py
@@ -10,7 +10,18 @@ import math
 from pprint import pprint
 
 
-def dfs(path, node, graph):
+def check_path(path):
+    lowers = set()
+    for n in path:
+        if n in lowers:
+            return False
+        if n not in ["start", "end"] and n.lower() == n:
+            lowers.add(n)
+
+    return True
+
+
+def dfs(path, node, graph, p2):
     paths = []
     for n in graph[node]:
         if n == "end":
@@ -18,14 +29,18 @@ def dfs(path, node, graph):
         elif n == "start":
             pass
         else:
-            # if n not in path:
             if n.upper() == n or n not in path:
-                paths.extend(dfs(path + [n], n, graph))
+                paths.extend(dfs(path + [n], n, graph, p2))
+            else:
+                # lowercase, possible duplicates
+                # make sure no other twos
+                if p2 and check_path(path):
+                    paths.extend(dfs(path + [n], n, graph, p2))
 
     return paths
 
 
-def p1(lines):
+def p1(lines, p2=False):
     graph = {}
     for edge in lines:
         begin, end = edge.split("-")
@@ -40,16 +55,16 @@ def p1(lines):
             if end != "end":
                 graph[end] = [begin]
 
-    return len(list(dfs(["start"], "start", graph)))
+    return len(list(dfs(["start"], "start", graph, p2)))
 
 
 def p2(lines):
-    pass
+    return p1(lines, True)
 
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         lines = [l.strip() for l in f.readlines()]
 
-    print(p1(lines))
-    # print(p2(lines))
+    # print(p1(lines))
+    print(p2(lines))
