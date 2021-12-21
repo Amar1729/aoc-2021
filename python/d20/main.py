3febef2702ffa1a4f0ba290857ad39b4324bd285
Amar1729 <amar.paul16@gmail.com>
2021-12-21 02:47:54 -0500

    d20p2

--- python/d20/main.py
+++ python/d20/main.py
@@ -1,148 +1,60 @@
 #! /usr/bin/env python3
 
-# 5438
-# 6301: too high
-# 5788: too high
-# 5401
-
 import sys
 
 
-def print_grid(grid):
-    points_x = sorted([int(k.real) for k in grid])
-    min_x = points_x[0]
-    max_x = points_x[-1]
-
-    points_y = sorted([int(k.imag) for k in grid])
-    min_y = points_y[0]
-    max_y = points_y[-1]
-
-    for j in range(min_y, max_y + 1):
-        for i in range(min_x, max_x + 1):
-            if complex(i, j) in grid:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-
-
-def dbg(grid):
-    points_x = sorted([int(k.real) for k in grid])
-    min_x = points_x[0]
-    max_x = points_x[-1]
-
-    points_y = sorted([int(k.imag) for k in grid])
-    min_y = points_y[0]
-    max_y = points_y[-1]
-
-    print("size of grid: ", min_x, max_x, min_y, max_y)
-    print_grid(grid)
-
-
-def get_surrounding(x: int, y: int, grid: set[complex], default_pixel: str) -> int:
-    if default_pixel == ".":
-        non_default_value = "1"
-        default_value = "0"
-    else:
-        non_default_value = "0"
-        default_value = "1"
-
+def get_surrounding(x: int, y: int, grid: dict[complex, str], default_pixel: str) -> int:
     surrounding = ""
     for j in [-1, 0, 1]:
         for i in [-1, 0, 1]:
-            surrounding += non_default_value if complex(x + i, y + j) in grid else default_value
+            surrounding += grid.get(complex(x + i, y + j), default_pixel)
 
+    surrounding = surrounding.replace(".", "0").replace("#", "1")
     return int(surrounding, 2)
 
 
-def complement_grid(grid):
-    # flip a grid from '#' to '.'
-    points_x = sorted([int(k.real) for k in grid])
-    min_x = points_x[0]
-    max_x = points_x[-1]
-
-    points_y = sorted([int(k.imag) for k in grid])
-    min_y = points_y[0]
-    max_y = points_y[-1]
-
-    new_grid = set()
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            if complex(x, y) not in grid:
-                new_grid.add(complex(x, y))
-
-    return new_grid
-
-
-def step(grid: set[complex], transform: str) -> set[complex]:
-    # also have to track the possible pixel values that the pixels at infinity
-    # might transform to (. -> # is possible if transform[0] == '#', etc)
-    start_pixel = transform[0]
-    end_pixel = transform[-1]
-
-    # assume pixels at infinity are dark
-    default_pixel = "."
-    # default_pixel = "#" if start_pixel == "." else "."
-
-    # print_grid(grid)
-    print("size of input: ", len(lines[2]), len(lines[2:]))
-    print()
-
+def step(grid: dict[complex, str], transform: str, default_pixel) -> dict[complex, str]:
     points_x = sorted([int(k.real) for k in grid])
     points_y = sorted([int(k.imag) for k in grid])
     rx = range(points_x[0] - 1, points_x[-1] + 2)
     ry = range(points_y[0] - 1, points_y[-1] + 2)
 
-    new_grid = set()
+    new_grid = {}
     for y in ry:
         for x in rx:
-            # new_pixel = transform[get_surrounding(x, y)]
             new_pixel = transform[get_surrounding(x, y, grid, default_pixel)]
-            # print(x, y, ": ", ("#" if complex(x, y) in grid else "."), new_pixel)
-            if new_pixel != default_pixel:
-                new_grid.add(complex(x, y))
-
-    if default_pixel == "#":
-        dbg(complement_grid(new_grid))
-    else:
-        dbg(new_grid)
-    # print_grid(new_grid)
-    print()
-
-    if default_pixel == ".":
-        if start_pixel == "#":
-            default_pixel = "#"
-
-    # if default_pixel == "#":
-    else:
-        if end_pixel == ".":
-            default_pixel = "."
+            new_grid[complex(x, y)] = new_pixel
 
     return new_grid
 
 
-def p1(lines):
+def p1(lines, steps=2):
     transform = lines[0]
 
     grid = {
-        complex(i, j)
+        complex(i, j): value
         for j, row in enumerate(lines[2:])
         for i, value in enumerate(row)
-        if value == "#"
     }
 
-    for _ in range(2):
-        grid = step(grid, transform)
+    for _ in range(steps // 2):
+        default_pixel = "."
+        grid = step(grid, transform, default_pixel=default_pixel)
+        if transform[0] == "#" and transform[-1] == ".":
+            default_pixel = "#"
+        grid = step(grid, transform, default_pixel=default_pixel)
 
-    return len(grid)
+    return len(list(filter(lambda p: p == "#", grid.values())))
 
 
 def p2(lines):
-    pass
+    # apparently, the unoptimized version (keeping track of all pixels =
+    # instead of ~half) works fine for p2 (~3.5sec)
+    return p1(lines, 50)
 
 
 if __name__ == "__main__":
     lines = open(sys.argv[1]).read().splitlines()
 
-    print(p1(lines))
-    # print(p2(lines))
+    # print(p1(lines))
+    print(p2(lines))
